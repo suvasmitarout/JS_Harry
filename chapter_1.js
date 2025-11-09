32a11f779c6f1d171ea1fdb53af8e2c48042207a
suvasmitarout <suvasmitarout97@gmail.com>
2025-11-09 16:38:25 +0530

    js learn

---
+++ chapter_1.js
@@ -0,0 +1,31 @@
+console.log("Operators in js")
+let a=45;
+let b=4;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+console.log("a++=",a++)
+console.log("a--=",a--)
+console.log("++a=",++a)
+console.log("a=",a)
+console.log("--a=",--a)
+
+// comparison operators
+let comp1=6;
+let comp2=5;
+console.log("comp1==comp2 is ",comp1==comp2)
+console.log("comp1!=comp2 is ",comp1!=comp2)
+console.log("comp1===comp2 is ",comp1===comp2)
+console.log("comp1!==comp2 is ",comp1!==comp2)
+console.log("comp1>comp2 is ",comp1>comp2)
+
+// Logical operator
+let x=5;
+let y=6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
