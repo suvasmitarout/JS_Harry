e7aa559e177d5df27fc2544d23d64a73a96d6482
suvasmitarout <suvasmitarout97@gmail.com>
2025-11-09 08:42:34 +0530

    change the sentences

---
+++ Practice_1.js
@@ -0,0 +1,26 @@
+// Question-1
+let a="Harry"
+let b=6
+console.log(a+b)
+//Question-2
+console.log(typeof (a+b))
+
+// Question-3
+const a1={
+    name:"Harry",
+    section:1,
+    isPrincipal:false
+}
+// a1=45
+// Question-4
+a1['friend']="Shubham"
+a1['name']="Lovish" 
+console.log(a1)
+// Question-5
+const dict={
+    appreciate:"recognize the full worth of.",
+    ataraxia:"a state of freedom from imotional disturbance and anxiety",
+    yakka:"work,especially hard work."
+}
+
+console.log(dict.yakka)
