06433864ccce665ae6f268d6ad15c0135e91b310
suvasmitarout <suvasmitarout97@gmail.com>
2025-11-14 09:07:56 +0530

    first commit

--- chapter_3.js
+++ chapter_3.js
@@ -1,9 +1,20 @@
 // Program to add first n natural number
-let sum=0
-let n=prompt("Enter the value of n")
-n=Number .parseInt(n)
-for(let i=0;i<n;i++){
-    sum+=(i+1)
-}
+// let sum=0
+// let n=prompt("Enter the value of n")
+// n=Number .parseInt(n)
+// for(let i=0;i<n;i++){
+//     sum+=(i+1)
+// }
 
-console.log("Sum of first "+ n + " natural number is " + sum)
+// console.log("Sum of first "+ n + " natural number is " + sum)
+
+let obj={
+    harry:90,
+    shubh:45,
+    shivika:56,
+    ritika:57,
+    shiv:23
+}
+for(let a in obj){
+    console.log("Marks of " + a +" are " + obj[a])
+}
