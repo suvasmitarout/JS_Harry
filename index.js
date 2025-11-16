a677f090ded8b6654d944fa8f5f0eec222ceaa0d
suvasmitarout <suvasmitarout97@gmail.com>
2025-11-16 09:58:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let name="Harry"
+console.log(name[0])
+console.log(name[1])
+let friend='Prakash'
+console.log(friend)
+
+// Template literals
+
+let boy1="Pramod"
+let boy2="Nikhil"
+// Nikhil is a friend of Pramod
+let sentence=`${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+let fruit='Bana\nna'
+console.log(fruit)
